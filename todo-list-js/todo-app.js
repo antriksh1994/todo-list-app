856fcbfbd95d8996fe5c0ceb9026daa85caa26ec
Antriksh Prasad <antrikshprasad@Antrikshs-MacBook-Air.local>
2026-05-16 21:03:02 +0530

    feat: add Todo list add edit

--- todo-list-js/todo-app.js
+++ todo-list-js/todo-app.js
@@ -1,6 +1,6 @@
 const state = {
-  taskName: "",
-  tasks: [],
+    taskName: "",
+    tasks: [],
 };
 
 const getInput = () => document.querySelector(".task-input");
@@ -9,64 +9,73 @@ const getTask = () => document.querySelectorAll(".task");
 loadTasksFromLocalStorage();
 
 function handleInputChange(e) {
-  state.taskName = e.target.value;
+    state.taskName = e.target.value;
 }
 
 function handleAddTask() {
-  if (!state.taskName.trim()) {
-    alert("Task cannot be empty");
-    return;
-  }
-  state.tasks.push({ title: state.taskName.trim(), isCompleted: false });
-  renderTasks();
-  getInput().value = "";
-  saveTasksToLocalStorage();
-  console.log("Tasks:", state.tasks);
+    if (!state.taskName.trim()) {
+        alert("Task cannot be empty");
+        return;
+    }
+    state.tasks.push({ title: state.taskName.trim(), isCompleted: false });
+    renderTasks();
+    getInput().value = "";
+    saveTasksToLocalStorage();
+    console.log("Tasks:", state.tasks);
 }
 
 function renderTasks() {
-  const tasksContainer = getTasksContainer();
-  tasksContainer.innerHTML = "";
-  state.tasks.forEach((task, index) => {
-    const taskElement = document.createElement("div");
-    const taskDeleteBtnContainer = document.createElement("span");
-    taskElement.className = "task";
-    taskDeleteBtnContainer.className = "delete-btn-container";
-    taskElement.innerHTML = `
+    const tasksContainer = getTasksContainer();
+    tasksContainer.innerHTML = "";
+    state.tasks.forEach((task, index) => {
+        const taskElement = document.createElement("li");
+        const taskDeleteBtnContainer = document.createElement("span");
+        const editbtn = document.createElement("span");
+        taskElement.className = "task";
+        taskDeleteBtnContainer.className = "delete-btn-container";
+        editbtn.className = "edit-btn";
+        taskElement.innerHTML = `
             <input type="checkbox" id="checkbox-${index}" onclick="toggleCompletion(${index})" ${task.isCompleted ? "checked" : ""}>
             <p class="task-text ${task.isCompleted ? "completed" : ""}">${task.title}</p>
         `;
-    taskDeleteBtnContainer.innerHTML = `<button class="delete-task-btn" onclick="deleteTask(${index}, '${task.title}')">X</button>`;
-    taskElement.appendChild(taskDeleteBtnContainer);
-    tasksContainer?.appendChild(taskElement);
-  });
+        taskDeleteBtnContainer.innerHTML = `<button class="delete-task-btn" onclick="deleteTask(${index}, '${task.title}')">X</button>`;
+        editbtn.innerHTML = `<button class="edit-task-btn" onclick="editTask(${index})">Edit</button>`;
+
+        taskElement.appendChild(editbtn);
+        taskElement.appendChild(taskDeleteBtnContainer);
+        tasksContainer.appendChild(taskElement);
+    });
 }
 
 function saveTasksToLocalStorage() {
-  localStorage.setItem("tasks", JSON.stringify(state.tasks));
+    localStorage.setItem("tasks", JSON.stringify(state.tasks));
 }
 
 function loadTasksFromLocalStorage() {
-  const tasksFromStorage = localStorage.getItem("tasks");
-  if (tasksFromStorage) {
-    state.tasks = JSON.parse(tasksFromStorage);
-    renderTasks();
-  }
+    const tasksFromStorage = localStorage.getItem("tasks");
+    if (tasksFromStorage) {
+        state.tasks = JSON.parse(tasksFromStorage);
+        renderTasks();
+    }
 }
 
 function deleteTask(index, task) {
-  console.log("index to delete", index);
-  console.log("task to delete", task);
-  state.tasks.splice(index, 1);
-  saveTasksToLocalStorage();
-  renderTasks();
+    console.log("index to delete", index);
+    console.log("task to delete", task);
+    state.tasks.splice(index, 1);
+    saveTasksToLocalStorage();
+    renderTasks();
 }
-
+function editTask(index) {
+    state.tasks[index].title = prompt("Edit task:", state.tasks[index].title);
+    saveTasksToLocalStorage();
+    renderTasks();
+}
 function toggleCompletion(index) {
-  console.log("getTask()", getTask());
-  const checkboxChecked = document.getElementById(`checkbox-${index}`).checked;
-  console.log("checkbox with index", checkboxChecked);
-  getTask()[index].classList.toggle("completed", Boolean(checkboxChecked));
-  state.tasks[index].isCompleted = checkboxChecked;
-  saveTasksToLocalStorage();
+    console.log("getTask()", getTask());
+    const checkboxChecked = document.getElementById(`checkbox-${index}`).checked;
+    console.log("checkbox with index", checkboxChecked);
+    getTask()[index].classList.toggle("completed", Boolean(checkboxChecked));
+    state.tasks[index].isCompleted = checkboxChecked;
+    saveTasksToLocalStorage();
 }
